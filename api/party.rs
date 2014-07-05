8cf0602736475946a1fdb8adb60571e0582204ad
Jonas Hietala <tradet.h@gmail.com>
2014-07-05 20:47:03 +0200

    Party structure.

--- api/party.rs
+++ api/party.rs
@@ -5,5 +5,23 @@ use api::quest::*;
 pub struct Party {
     // ordering
     pub quest: Quest, // TODO should be optional!
+    // leader
+    // memberCount
+    // members
+    // name
+}
+
+#[deriving(Show, Encodable, Decodable)]
+pub struct PartyMember {
+    //
+    // achievements
+    // items
+    // profile
+    //      name
+    // stats
+    // auth
+    //    timestamps
+    //        created
+    //        loggedin
 }
 
